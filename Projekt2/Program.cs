ba939956c119c5b93a65303d68edfd7abb0e1caa
PCissewski <cissek99@gmail.com>
2021-12-08 21:21:31 +0100

    Starting new project

---
+++ Projekt2/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Projekt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
---
+++ Projekt2/record/Record.cs
@@ -0,0 +1,8 @@
+﻿namespace Projekt2.record
+{
+    public class Record
+    {
+        private string _person;
+        private int _key;
+    }
+}
